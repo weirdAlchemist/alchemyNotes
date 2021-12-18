08d8a72d2545bc3f5006e1aee7e9f1d0cba0e2aa
Sebastian Rapp <rappsebastian@gmx.net>
2021-12-18 22:40:01 +0100

    initial basic frontend files
    some js files + views

---
+++ AlchemyNotes/AlchemyNotes/wwwroot/js/Classes.js
@@ -0,0 +1,61 @@
+﻿Type = {
+    pen: "Pen",
+    marker: "Marker"
+};
+class Pen {
+    
+    constructor(color, size, type = Type.pen) {
+        this.color = color;
+        this.size = size;
+        this.type = type;
+        
+    };  
+};
+
+
+class Tool { };
+
+class Layer {
+    history = [];
+    constructor(guid, index, name, canvas) {
+        this.guid = guid;
+        this.index = index;
+        this.name = name;
+        this.canvas = canvas;
+    };
+
+};
+
+class User {
+    guid = null;
+    name = null;
+    currentPen = null;
+
+    constructor(guid, name, currentPen) {
+        this.guid = guid;
+        this.name = name;
+        this.currentPen = currentPen;
+    };
+};
+
+class DrawAction {
+    actionUser = null;
+    pen = null;
+    points = [];
+    layer = Null;
+
+    constructor(user) {
+        this.actionUser = user;
+    };
+
+    isValid() {
+        //TODO: validate
+        return true;
+    };
+
+    reverse() { };
+};
+
+class MoveAction {
+
+}
---
+++ AlchemyNotes/AlchemyNotes/wwwroot/js/NoteApp.js
@@ -0,0 +1,30 @@
+﻿
+const noteApp = Vue.createApp({
+    data() {
+        return {
+            pens: [],
+            blub: 'lol'
+        }
+    },
+    computed: {},
+    watch: {
+        pens(newPens) {
+            if (newPens.length == 0)
+                this.pens.push(this.getStandardBlackPen());
+        },
+        blub(newVal, oldVal) {
+            console.log(newVal);
+        }
+    },
+    methods: {
+        getStandardBlackPen() {
+            return new Pen("#000000", 1);
+        },
+
+        changeVal() {
+            this.blub = "xxx";
+        }
+    }
+}).mount('#noteapp');
+
+
